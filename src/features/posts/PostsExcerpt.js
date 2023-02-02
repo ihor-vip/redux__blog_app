0cee270167f1339c447ca559184b6dd12cbafe9f
ihor-vip <vip20140902@gmail.com>
2023-02-02 12:44:36 +0200

    remove strict mode in index.js, as displaying posts twice

--- src/features/posts/PostsExcerpt.js
+++ src/features/posts/PostsExcerpt.js
@@ -2,16 +2,16 @@ import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 
-const PostsExcerpt = ({ post }) => {
+const PostsExcerpt = ({post}) => {
     return (
         <article>
             <h3>{post.title}</h3>
             <p>{post.body.substring(0, 100)}</p>
             <p className="postCredit">
-                <PostAuthor userId={post.userId} />
-                <TimeAgo timestamp={post.date} />
+                <PostAuthor userId={post.userId}/>
+                <TimeAgo timestamp={post.date}/>
             </p>
-            <ReactionButtons post={post} />
+            <ReactionButtons post={post}/>
         </article>
     )
 }
