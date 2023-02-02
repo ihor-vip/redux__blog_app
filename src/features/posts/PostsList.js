be26d983deb83a83c42875316c9b1e8d1e692390
ihor-vip <vip20140902@gmail.com>
2023-02-02 10:00:23 +0200

    add time when post been made, date-fns library

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,6 +1,7 @@
 import {useSelector} from "react-redux";
 import {selectAllPosts} from "./postsSlice";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
 
@@ -13,6 +14,7 @@ const PostsList = () => {
 
             <p className="postCredit">
                 <PostAuthor userId={post.userId}/>
+                <TimeAgo timestamp={post.date}/>
             </p>
         </article>
     ))
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,18 @@
 import {createSlice, nanoid} from "@reduxjs/toolkit";
+import {sub} from "date-fns";
 
 const initialState = [
     {
         id: '1',
         title: 'Life is awesome',
-        content: "There's more to life than stupid boys, you know"
+        content: "There's more to life than stupid boys, you know",
+        date: sub(new Date(), {minutes: 10}).toISOString(),
     },
     {
         id: '2',
         title: 'Winter is coming...',
-        content: "Don't call me Lord Snow"
+        content: "Don't call me Lord Snow",
+        date: sub(new Date(), {minutes: 5}).toISOString(),
     }
 ]
 
@@ -27,7 +30,8 @@ const postsSlice = createSlice({
                         id: nanoid(),
                         title,
                         content,
-                        userId
+                        userId,
+                        date: new Date().toISOString()
                     }
                 }
             }
