7074dfad56b89f63fb26a000e58a9513b87a5fa6
ihor-vip <vip20140902@gmail.com>
2023-02-06 13:58:03 +0200

    create edit post component, add EditPost reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -20,6 +20,16 @@ export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPos
     return response.data
 })
 
+export const updatePost = createAsyncThunk('posts/updatePost', async (initialPost) => {
+    const {id} = initialPost;
+    try {
+        const response = await axios.put(`${POSTS_URL}/${id}`, initialPost)
+        return response.data
+    } catch (err) {
+        return err.message
+    }
+})
+
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
@@ -107,6 +117,17 @@ const postsSlice = createSlice({
                 }
                 console.log(action.payload)
                 state.posts.push(action.payload)
+            })
+            .addCase(updatePost.fulfilled, (state, action) => {
+                if (!action.payload?.id) {
+                    console.log('Update could not complete')
+                    console.log(action.payload)
+                    return;
+                }
+                const { id } = action.payload;
+                action.payload.date = new Date().toISOString();
+                const posts = state.posts.filter(post => post.id !== id);
+                state.posts = [...posts, action.payload];
             })
     }
 })
