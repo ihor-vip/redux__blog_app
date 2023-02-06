1afc908ca538f90a7b60490bfd0307f7b842db9c
ihor-vip <vip20140902@gmail.com>
2023-02-06 16:05:52 +0200

    delete function been added

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -30,6 +30,17 @@ export const updatePost = createAsyncThunk('posts/updatePost', async (initialPos
     }
 })
 
+export const deletePost = createAsyncThunk('posts/deletePost', async (initialPost) => {
+    const {id} = initialPost;
+    try {
+        const response = await axios.delete(`${POSTS_URL}/${id}`)
+        if (response?.status === 200) return initialPost;
+        return `${response?.status}: ${response?.statusText}`;
+    } catch (err) {
+        return err.message;
+    }
+})
+
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
@@ -124,10 +135,20 @@ const postsSlice = createSlice({
                     console.log(action.payload)
                     return;
                 }
-                const { id } = action.payload;
+                const {id} = action.payload;
                 action.payload.date = new Date().toISOString();
                 const posts = state.posts.filter(post => post.id !== id);
                 state.posts = [...posts, action.payload];
+            })
+            .addCase(deletePost.fulfilled, (state, action) => {
+                if (!action.payload?.id) {
+                    console.log('Delete could not complete')
+                    console.log(action.payload)
+                    return;
+                }
+                const {id} = action.payload;
+                const posts = state.posts.filter(post => post.id !== id);
+                state.posts = posts;
             })
     }
 })
