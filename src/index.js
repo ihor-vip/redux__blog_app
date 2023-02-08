3e3fc7342511ee4c9129b597600bed94268b3ca9
ihor-vip <vip20140902@gmail.com>
2023-02-08 11:38:53 +0200

    initialisation of new project.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {store} from './app/store';
-import {Provider} from 'react-redux';
-import {fetchUsers} from "./features/users/usersSlice";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {fetchPosts} from "./features/posts/postsSlice";
 
-store.dispatch(fetchPosts())
-store.dispatch(fetchUsers())
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Provider store={store}>
-        <Router>
-            <Routes>
-                <Route path="/*" element={<App/>}/>
-            </Routes>
-        </Router>
-    </Provider>
-);
+ReactDOM.createRoot(document.getElementById('root'))
+    .render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
