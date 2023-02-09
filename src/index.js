c187ae13dde331a0b60286e86d2618c012506d95
ihor-vip <vip20140902@gmail.com>
2023-02-09 17:19:33 +0200

    check out old project

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {store} from './app/store';
+import {Provider} from 'react-redux';
+import {fetchPosts} from './features/posts/postsSlice';
+import {fetchUsers} from './features/users/usersSlice';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-import { ApiProvider } from "@reduxjs/toolkit/query/react";
-import { apiSlice } from "./features/api/apiSlice";
+store.dispatch(fetchPosts());
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById('root'))
     .render(
         <React.StrictMode>
-            <ApiProvider api={apiSlice}>
-                <App />
-            </ApiProvider>
+            <Provider store={store}>
+                <Router>
+                    <Routes>
+                        <Route path="/*" element={<App/>}/>
+                    </Routes>
+                </Router>
+            </Provider>
         </React.StrictMode>
     );
