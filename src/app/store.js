3db8dc7df63e1420e3efae479cb67fdc187f1151
ihor-vip <vip20140902@gmail.com>
2023-02-09 17:54:34 +0200

    refactoring index.js for RTK Query

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from '../features/api/apiSlice';
+import usersReducer from '../features/users/usersSlice'
 
 export const store = configureStore({
     reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        users: usersReducer
     },
     middleware: getDefaultMiddleware =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: true
+        getDefaultMiddleware().concat(apiSlice.middleware)
 })
