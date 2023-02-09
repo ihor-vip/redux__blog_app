590dbb1c62f2831e4e504ad7e285543a74562285
ihor-vip <vip20140902@gmail.com>
2023-02-09 18:23:27 +0200

    refactoring AddPostForm.js for RTK Query

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,19 +1,17 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-import { addNewPost } from "./postsSlice";
+import {  useSelector } from "react-redux";
 import { selectAllUsers } from "../users/usersSlice";
 import { useNavigate } from "react-router-dom";
+import {useAddNewPostMutation} from "./postsSlice";
 
 const AddPostForm = () => {
-    const dispatch = useDispatch()
+    const [addNewPost, { isLoading }] = useAddNewPostMutation()
 
     const navigate = useNavigate()
 
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     const [userId, setUserId] = useState('')
-    const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
     const users = useSelector(selectAllUsers)
 
@@ -22,13 +20,12 @@ const AddPostForm = () => {
     const onAuthorChanged = e => setUserId(e.target.value)
 
 
-    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
+    const canSave = [title, content, userId].every(Boolean) && !isLoading;
 
-    const onSavePostClicked = () => {
+    const onSavePostClicked = async () => {
         if (canSave) {
             try {
-                setAddRequestStatus('pending')
-                dispatch(addNewPost({ title, body: content, userId })).unwrap()
+                await addNewPost({ title, body: content, userId }).unwrap()
 
                 setTitle('')
                 setContent('')
@@ -36,11 +33,8 @@ const AddPostForm = () => {
                 navigate('/')
             } catch (err) {
                 console.error('Failed to save the post', err)
-            } finally {
-                setAddRequestStatus('idle')
             }
         }
-
     }
 
     const usersOptions = users.map(user => (
